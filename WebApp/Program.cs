2982069a2f9a6e237d3c00cc660bb6d87f9246f1
peanut-lover <jihoson94@gmail.com>
2023-04-28 17:11:42 +0900

    DB Connection Test

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -26,14 +26,14 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
 }
-else
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
